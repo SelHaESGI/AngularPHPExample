102b1513297050efc89b855ad9825212b8f92803
SelHaESGI <selim.ham75@gmail.com>
2022-02-22 13:25:05 +0100

    Adding php content

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+if ($_POST['name']) {
+  $res = "Hi " . $_POST['name'];
+  echo json_encode($res);
+}
+
+?>
